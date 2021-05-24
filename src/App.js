44d8d3899cb0206d8c521fe990e10019d820be9f
iceman121212 <9387729+iceman121212@users.noreply.github.com>
2021-05-24 14:35:10 -0600

    tutorial 2 complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import Header from './components/Header';
+import { Route, Switch } from 'react-router';
 import './default.scss'
+import HomepageLayout from './layouts/HomepageLayout';
+import MainLayout from './layouts/MainLayout';
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => (
+          <HomepageLayout>
+            <Homepage />
+          </HomepageLayout>
+        )} />
+        <Route path='/registration' render={() => (
+          <MainLayout>
+            <Registration />
+          </MainLayout>
+        )} />
+      </Switch>
     </div>
   )
 }
