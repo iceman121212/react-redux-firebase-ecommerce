d071c77a22af660915e953ec6d805b6a740d6c9f
iceman121212 <9387729+iceman121212@users.noreply.github.com>
2021-06-04 13:38:08 -0600

    tutorial-18-DC-attempt complete

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -4,6 +4,8 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from "redux-logger";
 import thunk from 'redux-thunk'
 
+import { persistStore } from 'redux-persist'
+
 import rootReducer from "./rootReducer";
 import rootSaga from './rootSaga'
 
@@ -14,4 +16,10 @@ export const middlewares = [thunk, sagaMiddleware, logger]
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
 sagaMiddleware.run(rootSaga)
 
-export default store
+export const persistor = persistStore(store)
+
+export default {
+  store,
+  persistor,
+}
+
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,8 +3,19 @@ import cartReducer from "./Cart/cart.reducer";
 import productsReducer from "./Products/products.reducer";
 import userReducer from "./User/user.reducer";
 
-export default combineReducers({
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+export const rootReducer = combineReducers({
   user: userReducer,
   productsData: productsReducer,
   cartData: cartReducer,
 })
+
+const configStorage = {
+  key: 'root',
+  storage,
+  whitelist: ['cartData'],
+}
+
+export default persistReducer(configStorage, rootReducer)
